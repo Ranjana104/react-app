823385fc8b21b0b798eef4b018a8f34e999bed43
unknown <RAMARJUN397>
2022-09-13 10:15:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ function App() {
           <h1> Login Page </h1>
           <div>
             <img src={email} alt="email" className="email"/>
-            <input type="text" placeholder="user name" className="name"/>
+            <input type="text" placeholder="Username" className="name"/>
           </div> 
           <div className="second-input">
             <img src={pass} alt="pass" className="pass"/>
-            <input type="text" placeholder="password" className="name"/>
+            <input type="password" placeholder="Password" className="name"/>
           </div> 
           <div className="login-button">
             <button> Login </button>
